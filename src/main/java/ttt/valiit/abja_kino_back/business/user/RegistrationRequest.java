71f9cbc008c285c10e1e14161f03f8ae25062c4f
TuuliRei <reiman.tuuli@gmail.com>
2023-05-17 16:40:17 +0300

    Tuuli registration WIP

--- src/main/java/ttt/valiit/abja_kino_back/business/user/RegistrationRequest.java
+++ src/main/java/ttt/valiit/abja_kino_back/business/user/RegistrationRequest.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
+import lombok.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ttt.valiit.abja_kino_back.domain.user.User;
 
 import java.io.Serializable;
@@ -12,17 +13,22 @@ import java.io.Serializable;
 /**
  * A DTO for the {@link User} entity
  */
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationRequest implements Serializable {
     @Size(max = 255)
     @NotNull
-    private final String username;
+    private String username;
     @Size(max = 255)
     @NotNull
     @Min(value = 8, message = "Password must be at least 8 characters long")
-    private final String password;
+    private String password;
     @Size(max = 255)
     @NotNull
     @Email(message = "Email should be valid")
-    private final String email;
+    private String email;
+
+
 }
