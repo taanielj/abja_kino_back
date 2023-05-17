3ecf5ddf9dab8ea8973018c9ccd8a4b32d19154b
TIINA <tiinake.normak@gmail.com>
2023-05-17 16:54:19 +0300

    Added Ticket Controller

--- database/import.sql
+++ database/import.sql
@@ -1,6 +1,8 @@
-INSERT INTO public.role (id, name) VALUES (DEFAULT, 'ROLE_ADMIN');
-INSERT INTO public.role (id, name) VALUES (DEFAULT, 'ROLE_CUSTOMER');
 
-INSERT INTO public."user" (id, username, password, role_id, email) VALUES (DEFAULT, 'admin', '123', 1, 'tuuli@abja.com');
-INSERT INTO public."user" (id, username, password, role_id, email) VALUES (DEFAULT, 'tuuli', '123', 2, 'tuuli2@abja.com');
+INSERT INTO public.genre (id, name) VALUES (1, 'komöödia');
+INSERT INTO public.genre (id, name) VALUES (2, 'seiklus');
+INSERT INTO public.genre (id, name) VALUES (3, 'draama');
 
+INSERT INTO public.ticket_type (id, name, price) VALUES (1, 'tava', 10.00);
+INSERT INTO public.ticket_type (id, name, price) VALUES (2, 'soodus', 8.00);
+INSERT INTO public.ticket_type (id, name, price) VALUES (3, 'klubipilet', 8.50);
