8c7490e37a516aa6addb6618a39c578fe0e44e14
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2023-05-24 17:05:53 +0300

    get active movie Ids kordatehtud.

--- src/main/java/ttt/valiit/abja_kino_back/business/movie/MovieService.java
+++ src/main/java/ttt/valiit/abja_kino_back/business/movie/MovieService.java
@@ -8,7 +8,6 @@ import ttt.valiit.abja_kino_back.domain.movie.Movie;
 import ttt.valiit.abja_kino_back.domain.movie.MovieMapper;
 import ttt.valiit.abja_kino_back.domain.movie.MovieRepository;
 import ttt.valiit.abja_kino_back.domain.seance.SeanceRepository;
-import ttt.valiit.abja_kino_back.infrastructure.exception.MovieTitleExistsException;
 import ttt.valiit.abja_kino_back.infrastructure.exception.DatabaseConstraintExcept;
 import ttt.valiit.abja_kino_back.infrastructure.exception.DatabaseNameConflictException;
 import ttt.valiit.abja_kino_back.infrastructure.exception.ResourceNotFoundException;
