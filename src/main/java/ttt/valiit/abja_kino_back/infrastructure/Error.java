f5bd75153bcd782e81ff31e9f5c6509c18890824
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2023-05-26 09:08:32 +0300

    added error types

---
+++ src/main/java/ttt/valiit/abja_kino_back/infrastructure/Error.java
@@ -0,0 +1,33 @@
+package ttt.valiit.abja_kino_back.infrastructure;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+
+@AllArgsConstructor
+@Getter
+public enum Error {
+    GENRE_NOT_FOUND("Žanri ei leitud"),
+    MOVIE_NOT_FOUND("Filmi ei leitud"),
+    ROOM_NOT_FOUND("Saali ei leitud"),
+    SEANCE_NOT_FOUND("Seanssi ei leitud"),
+    SEAT_NOT_FOUND("Kohta ei leitud"),
+    TICKET_TYPE_NOT_FOUND("Piletitüüpi ei leitud"),
+    TICKET_NOT_FOUND("Piletit ei leitud"),
+    USER_NOT_FOUND("Kasutajat ei leitud"),
+    GENRE_EXISTS("Žanr on juba olemas"),
+    MOVIE_EXISTS("Film on juba olemas"),
+    ROOM_EXISTS("Saal on juba olemas"),
+    SEANCE_EXISTS("Seanss on juba olemas"),
+    SEAT_EXISTS("Koht on juba olemas"),
+    TICKET_TYPE_EXISTS("Piletitüüp on juba olemas"),
+    TICKET_EXISTS("Pilet on juba olemas"),
+    USER_EXISTS("Kasutaja on juba olemas"),
+    DATABASE_NAME_CONFLICT(" nimega on juba olemas"),
+    DATABASE_NAME_MUST_NOT_BE_EMPTY("Nimi ei tohi olla tühi"),
+    DATABASE_CONSTRAINT("Andmebaasi tervikluse rikkumine"),
+    RESOURCE_NOT_FOUND("Ressurssi ei leitud");
+    private final String message;
+
+
+}
